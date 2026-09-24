dce4085fa9eb284563017cdcfbb6bbcf47017775
Md. Arifur Rahman <arifurroni@gmail.com>
2026-09-25 03:29:41 +0600

    Banner updated

--- src/components/homepage/Banner.tsx
+++ src/components/homepage/Banner.tsx
@@ -4,7 +4,7 @@ import bannerImage from '@/assets/banner.png';
 
 const BannerPage = () => {
     return (
-        <div className="flex flex-col md:flex-row items-center justify-between gap-10 rounded-2xl bg-[#15171D] p-4 md:p-10 bg-black text-white">
+        <div className="flex flex-col md:flex-row items-center justify-between gap-10 rounded-2xl bg-[#15171D] p-4 md:p-10 text-white">
             <div className="grid gap-4 md:w-1/2">
                 <p className='text-lime-400 text-[10px]'>WORKOUT LIBRARY</p>
             <h1 className='text-4xl font-bold'>TRAIN WITH INTENT. LOG <br /> EVERY SET.</h1>
